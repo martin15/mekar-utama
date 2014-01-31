a6b3ed12eaf1ffd90608601a8724b3c3d3e40e42
martin <martin.me15@yahoo.com>
2014-02-01 01:15:28 +0800

    change new syntax to old syntax

--- mekar_utama/config/initializers/session_store.rb
+++ mekar_utama/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MekarUtama::Application.config.session_store :cookie_store, key: '_mekar_utama_session'
+MekarUtama::Application.config.session_store :cookie_store, :key => '_mekar_utama_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
