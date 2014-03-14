675b092011bf29edd6792519f9f06d65f6b96a46
martin <martin.me15@yahoo.com>
2014-03-15 01:05:55 +0800

    handling error for invalid product

--- mekar_utama/app/controllers/products_controller.rb
+++ mekar_utama/app/controllers/products_controller.rb
@@ -1,21 +1,28 @@
 class ProductsController < ApplicationController
+  before_filter :find_category, :only => [:index, :show]
 
   def index
-    @category = Category.includes(:products).find_by_name(params[:name])
-    if @category.nil?
-      flash[:error] = "Cannot find the category"
-      redirect_to root_path
-    end
+
     @products = @category.products.paginate(:page => params[:page], :per_page => 6,
                                  :order => "created_at DESC" )
   end
 
   def show
-    @product = Product.find_by_id(params[:id])
+    @product = @category.products.find_by_id(params[:id])
     if @product.nil?
       flash[:error] = "Cannot find the product"
       redirect_to root_path
+      return
     end
     @product_images = @product.product_images
   end
+
+  private
+    def find_category
+      @category = Category.includes(:products).find_by_name(params[:name])
+      if @category.nil?
+        flash[:error] = "Cannot find the category"
+        redirect_to root_path
+      end
+    end
 end
