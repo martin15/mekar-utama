5c3e5f73da8e613b085d5b395ea7f7afdd6a8323
martin <martin.me15@yahoo.com>
2014-09-11 15:15:31 +0800

    fixing news page handling for content

--- mekar_utama/app/helpers/application_helper.rb
+++ mekar_utama/app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def short_desc(text, count)
     new_text_splited = text[0, count].split(" ")
-    new_text_splited.pop(1)
+    new_text_splited.pop(1) unless new_text_splited.count == 1
     return new_text_splited.join(" ")
   end
 end
